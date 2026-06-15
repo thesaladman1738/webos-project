2c3bf5af92f8fe266355372866749e803beb0c69
thesaladman1738 <thesaladman1738@proton.me>
2026-06-15 15:25:42 +0000

    cleaned up css and added new buttons

--- script.js
+++ script.js
@@ -47,3 +47,25 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
+
+var welcomeScreen = document.querySelector("#window")
+
+function closeWindow(element) {
+  element.style.display = "none"
+}
+
+function openWindow(element) {
+  element.style.display = "flex"
+}
+
+var welcomeScreenClose = document.querySelector("#welcomeclose")
+
+var welcomeScreenOpen = document.querySelector("#welcomeopen")
+
+welcomeScreenClose.addEventListener("click",function() {
+  closeWindow(welcomeScreen);
+});
+
+welcomeScreenOpen.addEventListener("click", function() {
+  openWindow(welcomeScreen);
+});
